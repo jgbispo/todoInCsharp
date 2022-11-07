96c5d00b178d37d67ace1f5346038ff061a8b338
bshop <jgbispo20@gmail.com>
2022-11-07 13:17:50 -0300

    New Era

--- Program.cs
+++ Program.cs
@@ -1,25 +1,10 @@
-﻿using ToDoSpace;
-using Screens;
-namespace Main
+﻿namespace Main
 {
   class Program
   {
     static void Main(string[] args)
     {
-      // Create a new Controller
-      Controller c = new Controller();
-
-      // Create a new ToDo
-      ToDo t = CreateToDo.CreateToDoScreen(c);
-
-      // Add the ToDo to the Controller
-      c.AddToDo(t);
-
-      // Create a new Item
-      Item i = CreateItem.CreateItemScreen(t);
-
-      // Add the Item to the ToDo
-      t.AddItem(i);
+      Console.WriteLine("Hello World!");
     }
   }
 }
