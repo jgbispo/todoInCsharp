50bda0701c16496105f2e2e32036737b2ee301e7
bshop <jgbispo20@gmail.com>
2022-11-07 15:03:17 -0300

    Database (tmp) and Controllers

---
+++ Controller.cs
@@ -0,0 +1,47 @@
+using src;
+
+namespace Main
+{
+  class Controller
+  {
+    public static Item CreateItem()
+    {
+      Console.Clear();
+      Console.WriteLine("Enter the name of the item:");
+      string name = Console.ReadLine()!;
+      Console.WriteLine("Enter the description of the item:");
+      string description = Console.ReadLine()!;
+      Item item = new Item(name, description);
+      return item;
+    }
+
+    public static ToDo CreateToDo()
+    {
+      Console.Clear();
+      Console.WriteLine("Enter the name of the ToDo:");
+      string name = Console.ReadLine()!;
+      Console.WriteLine("Enter the description of the ToDo:");
+      string description = Console.ReadLine()!;
+      ToDo toDo = new ToDo(name, description);
+      return toDo;
+    }
+
+    public static void ShowToDoList(Database database)
+    {
+      database.ShowToDoList();
+    }
+
+    public static void AddToDoToList(Database database, ToDo toDo)
+    {
+      database.AddToDo(toDo);
+    }
+
+    public static void RemoveToDoFromList(Database database)
+    {
+      Console.WriteLine("Enter the name of the ToDo you want to remove:");
+      string name = Console.ReadLine()!;
+      database.RemoveToDoByName(name);
+    }
+
+  }
+}
---
+++ Database.cs
@@ -0,0 +1,47 @@
+using src;
+
+namespace Main
+{
+  class Database
+  {
+    private List<ToDo> toDoList = new List<ToDo>();
+    public void AddToDo(ToDo toDo)
+    {
+      toDoList.Add(toDo);
+    }
+
+    public void ShowToDoList()
+    {
+      Console.WriteLine($"Name\tDescription\tItems");
+      Console.WriteLine("-------------------------------");
+      foreach (ToDo toDo in toDoList)
+      {
+        int count = toDo.Items.Count;
+        Console.WriteLine($"{toDo.Name}\t{toDo.Description}\t{count}");
+      }
+    }
+
+    public void RemoveToDoByName(string name)
+    {
+      for (int i = 0; i < toDoList.Count; i++)
+      {
+        if (toDoList[i].Name == name)
+        {
+          toDoList.RemoveAt(i);
+        }
+      }
+    }
+
+    public ToDo? FindToDoByName(string name)
+    {
+      foreach (ToDo toDo in toDoList)
+      {
+        if (toDo.Name == name)
+        {
+          return toDo;
+        }
+      }
+      return null;
+    }
+  }
+}
