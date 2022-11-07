37fecf545c6dd4490dce1439e566bb71b1634950
bshop <jgbispo20@gmail.com>
2022-11-07 09:57:36 -0300

    Item to ToDoList

---
+++ src/Item.cs
@@ -0,0 +1,23 @@
+namespace ToDO
+{
+  class Item
+  {
+    // Properties
+    private string? name;
+    private string? description;
+    private bool _isComplete;
+
+    // Constructor
+    public Item(string name, string description)
+    {
+      this.name = name;
+      this.description = description;
+      _isComplete = false;
+    }
+
+    // Getters and Setters
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsComplete { get; set; }
+  }
+}
